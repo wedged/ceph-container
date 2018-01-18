ee4c9acb17dab0d845e4a4ef9134dc380615632f
Sébastien Han <seb@redhat.com>
2018-01-18 14:59:59 +0100

    osd: add ceph-volume

--- ceph-releases/luminous/ubuntu/16.04/daemon/osd_scenarios/osd_disk_activate.sh
+++ ceph-releases/luminous/ubuntu/16.04/daemon/osd_scenarios/osd_disk_activate.sh
@@ -10,6 +10,7 @@ function osd_activate {
     exit 1
   fi
 
+  CEPH_ACTIVATE_BINARY=(ceph-volume)
   CEPH_DISK_OPTIONS=()
 
   if [[ ${OSD_FILESTORE} -eq 1 ]] && [[ ${OSD_DMCRYPT} -eq 0 ]]; then
@@ -38,21 +39,22 @@ function osd_activate {
   if [[ ${OSD_DMCRYPT} -eq 1 ]] && [[ ${OSD_FILESTORE} -eq 1 ]]; then
     get_dmcrypt_filestore_uuid
     mount_lockbox "$DATA_UUID" "$LOCKBOX_UUID"
+    CEPH_ACTIVATE_BINARY_CLI_OPTS+=('--dmcrypt')
     CEPH_DISK_OPTIONS+=('--dmcrypt')
     MOUNTED_PART="/dev/mapper/${DATA_UUID}"
     open_encrypted_parts_filestore
   elif [[ ${OSD_DMCRYPT} -eq 1 ]] && [[ ${OSD_BLUESTORE} -eq 1 ]]; then
     get_dmcrypt_bluestore_uuid
     mount_lockbox "$DATA_UUID" "$LOCKBOX_UUID"
-    CEPH_DISK_OPTIONS+=('--dmcrypt')
+    CEPH_ACTIVATE_BINARY_CLI_OPTS+=('--dmcrypt')
     MOUNTED_PART="/dev/mapper/${DATA_UUID}"
     open_encrypted_parts_bluestore
   fi
 
   if [[ -z "${CEPH_DISK_OPTIONS[*]}" ]]; then
-    ceph-disk -v --setuser ceph --setgroup disk activate --no-start-daemon "${DATA_PART}"
+    "${CEPH_ACTIVATE_BINARY[@]}" --setuser ceph --setgroup disk activate --no-start-daemon "${DATA_PART}"
   else
-    ceph-disk -v --setuser ceph --setgroup disk activate "${CEPH_DISK_OPTIONS[@]}" --no-start-daemon "${DATA_PART}"
+    "${CEPH_ACTIVATE_BINARY[@]}" --setuser ceph --setgroup disk activate "${CEPH_ACTIVATE_BINARY_CLI_OPTS[@]}" --no-start-daemon "${DATA_PART}"
   fi
 
   OSD_ID=$(grep "${MOUNTED_PART}" /proc/mounts | awk '{print $2}' | sed -r 's/^.*-([0-9]+)$/\1/')
